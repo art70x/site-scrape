8b9ea09037662ccaa7ebd8092b6914fa3cc5a74a
xy.art70 <xy.art70@gmail.com>
2025-06-14 12:43:44 +0000

    update(logo)

--- app/app.vue
+++ app/app.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 useSeoMeta({
-  viewport: 'width=device-width, initial-scale=1',
-  robots: 'index, follow, max-image-preview:large',
+  viewport: 'width=device-width, initial-scale=1.0',
+  charset: 'utf-8',
+  author: 'art70x',
   ogType: 'website',
-  ogSiteName: 'SiteScrape',
-  ogLocale: 'en_us',
+  ogLocale: 'en_US',
+  ogUrl: 'https://sitescrape.vercel.app/',
   twitterCard: 'summary_large_image',
+  themeColor: '#2b7fff',
 })
+
 useHead({
   htmlAttrs: {
     lang: 'en',
   },
+  titleTemplate: null,
   link: [{ rel: 'icon', type: 'image/svg+xml', href: '/icon.svg' }],
 })
 </script>
